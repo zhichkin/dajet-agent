c71141fe6f9c722011eda9af041017ab755b9180
zhichkin <dima_zhichkin@mail.ru>
2021-03-09 14:58:10 +0300

    Начало добавления поддержки PostgreSQL

---
+++ src/dajet-agent-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DaJet.Agent.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
